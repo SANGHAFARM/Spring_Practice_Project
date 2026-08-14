ff1143cf51cc5eb01333d7b871bf4a18dd128281
SANGHAFARM <sangha972@gmail.com>
2026-08-14 16:35:25 +0900

    Spring 실습 프로젝트 ch11까지 (pdf p.267까지)
    코드로 배우는 스프링 웹 프로젝트
    ch11 (pdf p.267까지) 진행

--- spring_workspace/ex02/src/main/java/org/zerock/controller/BoardController.java
+++ spring_workspace/ex02/src/main/java/org/zerock/controller/BoardController.java
@@ -40,10 +40,10 @@ public class BoardController {
 		return "redirect:/board/list";
 	}
 
-	@GetMapping("/get")
+	@GetMapping({"/get", "/modify"})
 	public void get(@RequestParam("bno") Long bno, Model model) {
 
-		log.info("/get");
+		log.info("/get or /modify");
 		model.addAttribute("board", service.get(bno));
 	}
 
@@ -56,16 +56,20 @@ public class BoardController {
 		}
 		return "redirect:/board/list";
 	}
-	
+
 	@PostMapping("/remove")
 	public String remove(@RequestParam("bno") Long bno, RedirectAttributes rttr) {
-		
+
 		log.info("remove..." + bno);
-		if (service.remove(bno))
-		{
+		if (service.remove(bno)) {
 			rttr.addFlashAttribute("result", "success");
 		}
-		
+
 		return "redirect:/board/list";
+	}
+
+	@GetMapping("/register")
+	public void register() {
+
 	}
 }
