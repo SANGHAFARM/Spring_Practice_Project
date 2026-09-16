0b31a940b8ecb84db82c4fa430fffd5585291986
SANGHAFARM <sangha972@gmail.com>
2026-09-16 17:35:49 +0900

    Spring 초급 실습 04 - HelloService를 @Service로 변경
    - HelloService의 @Component를 @Service로 변경
    - Service stereotype annotation 적용
    - hello() 메서드 유지
    - HelloPrinter의 생성자 주입 구조가 그대로 동작하는지 확인

--- spring_workspace/Practice/src/main/java/org/zerock/service/HelloService.java
+++ spring_workspace/Practice/src/main/java/org/zerock/service/HelloService.java
@@ -1,8 +1,8 @@
 package org.zerock.service;
 
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 public class HelloService {
 
     public String hello()
